5c31cfbe373c77fbc480af1661f80de13a6b3e25
Imad Mouhtassem <imad@imadm.ca>
2017-12-03 04:50:18 -0500

    Add heatmap points

--- heatmap/views.py
+++ heatmap/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import BikeShareHtmlInput
 import folium
+from folium import plugins
 import os.path
 import time
 import requests
@@ -21,6 +22,9 @@ def index(request):
             with open(api_file_path) as f:
                 data = json.load(f)
             station_data = data['data']['stations']
+            station_lat_long = {}
+            for station in station_data:
+                station_lat_long[station['name']] = {'lat': station['lat'], 'lon': station['lon']}
 
             user_entered_html = form.cleaned_data['html_paste']
             soup = BeautifulSoup(user_entered_html, 'html.parser')
@@ -38,7 +42,15 @@ def index(request):
                 station_trip_count[station_from] = station_trip_count.get(station_from, 0) + 1
                 station_trip_count[station_to] = station_trip_count.get(station_to, 0) + 1
 
+            heatmap_values = []
+            for key, value in station_trip_count.items():  # convert name:count to [lat, lon, count]
+                lat = station_lat_long[key]['lat']
+                lon = station_lat_long[key]['lon']
+                heatmap_values.append([lat, lon, value])
+
             m = folium.Map(location=[43.66093, -79.3880384], zoom_start=13.5, tiles='Stamen Toner')
+            m.add_children(plugins.HeatMap(heatmap_values, radius=15, blur=20))
+
             context['map'] = m._repr_html_()
     else:
         form = BikeShareHtmlInput()
