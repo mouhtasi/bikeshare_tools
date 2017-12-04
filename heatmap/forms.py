c4bd512f4fa07ed782cb22d1692387bf811a8176
Imad Mouhtassem <imad@imadm.ca>
2017-12-04 17:59:05 -0500

    Update heatmap page content to include details and explanation

--- heatmap/forms.py
+++ heatmap/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class BikeShareHtmlInput(forms.Form):
-    html_paste = forms.CharField(widget=forms.Textarea)
+    html_paste = forms.CharField(widget=forms.Textarea(attrs={'rows':'5'}))
