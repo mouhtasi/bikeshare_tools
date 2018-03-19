04120af1226e53fe67a33046e395e45c2171f2d0
Imad Mouhtassem <imad@imadm.ca>
2018-03-19 02:23:02 -0400

    Create Station model. Create management script to populate the stations database

---
+++ bikesharestationcatalog/management/commands/updatestationsdb.py
@@ -0,0 +1,32 @@
+from django.core.management.base import BaseCommand, CommandError
+from bikesharestationcatalog.models import Station
+import requests
+import json
+
+# TODO: Add a check to only update if data is changed
+
+class Command(BaseCommand):
+    help = 'Updates the Station table using API data'
+
+    def handle(self, *args, **options):
+        response = requests.get('https://tor.publicbikesystem.net/ube/gbfs/v1/en/station_information')
+        if response.status_code != requests.codes.ok:
+            raise CommandError('Failed to access API')
+
+        station_data = json.loads(response.content)['data']['stations']
+
+        num_updated = 0
+        num_created = 0
+        for station in station_data:
+            s, created = Station.objects.update_or_create(id=station['station_id'],
+                                                          defaults={'name': station['name'],
+                                                                    'longitude': station['lon'],
+                                                                    'latitude': station['lat'],
+                                                                    'capacity': station['capacity']})
+            if created:
+                num_created += 1
+            else:
+                num_updated += 1
+
+        self.stdout.write(self.style.SUCCESS('Stations updated. {} updated. {} created'.format(num_updated,
+                                                                                               num_created)))
--- bikesharestationcatalog/models.py
+++ bikesharestationcatalog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Station(models.Model):
+    """Based on the data from https://tor.publicbikesystem.net/ube/gbfs/v1/en/station_information"""
+    id = models.PositiveSmallIntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    capacity = models.PositiveSmallIntegerField()
