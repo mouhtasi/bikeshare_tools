5ed1f296209aca72fa6c8bdeb7794fd3607124bf
Imad Mouhtassem <imad@imadm.ca>
2018-03-26 17:39:27 -0400

    Add station catalog to the sidebar menu

--- bikesharestationcatalog/urls.py
+++ bikesharestationcatalog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.catalog_home, name='home'),
+    path('', views.catalog_home, name='station_catalog_home'),
     path('station_details/<int:s_id>/', views.station_details, name='station_details'),
 ]
